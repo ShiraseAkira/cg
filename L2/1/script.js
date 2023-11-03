f5418bf3bb24e73c6878f737121522c1983997d8
Maxim Laptev <justsaynya@gmail.com>
2023-11-03 21:42:29 +0300

    L2.1 implement TODOs

--- L2/1/script.js
+++ L2/1/script.js
@@ -1,14 +1,41 @@
+'use strict';
+
+function getImageBoundingBox(img) {
+    return [img.xCoord, img.xCoord + img.width, img.yCoord, img.yCoord + img.height];
+}
+
+function isCoordsInImageBoundingBox(img, x, y) {
+    const box = getImageBoundingBox(img);
+    if (x < box[0] ||
+        x > box[1] ||
+        y < box[2] ||
+        y > box[3]) 
+            {
+                return false;
+            }
+    return true;
+}
+
 window.addEventListener("load", () => {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
-    const img = new Image;
+    context.font = "20px sans-serif";
+    context.textBaseline = "top";
+    let images = [];
     let isDragging = false;
+    let draggingImage;
     let posX, posY;
 
     canvas.addEventListener('mousedown', (e) => {
-        posX = e.offsetX;
-        posY = e.offsetY;       
-        isDragging = true;
+        for(let i = 0; i < images.length; i++) {
+            if (isCoordsInImageBoundingBox(images[i], e.offsetX, e.offsetY)) {
+                draggingImage = images[i];
+                isDragging = true;
+                posX = e.offsetX;
+                posY = e.offsetY;  
+                break;
+            }
+        }        
     });
     canvas.addEventListener('mouseup', () => isDragging = false);
     canvas.addEventListener('mouseout', () => isDragging = false);
@@ -16,33 +43,52 @@ window.addEventListener("load", () => {
         if(isDragging) {
             let deltaX = e.offsetX - posX;
             let deltaY = e.offsetY - posY;
-            context.translate(deltaX, deltaY);
+            draggingImage.xCoord += deltaX;
+            draggingImage.yCoord += deltaY; 
             posX += deltaX;
             posY += deltaY;
+            draw();
         }
     });
 
     const fileInput = document.getElementById("imageFile");
     fileInput.addEventListener("change", (e) => {
-        img.addEventListener("load", drawImage);
-        img.src = URL.createObjectURL(e.target.files[0]);
+        images = [];
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < e.target.files.length; i++) {
+            const img = new Image;
+            img.xCoord = 0;
+            img.yCoord = 0;
+            img.fileName = e.target.files[i].name;
+            img.addEventListener("load", () => context.drawImage(img, img.xCoord, img.yCoord));
+            img.src = URL.createObjectURL(e.target.files[i]);
+            images.push(img);
+        }
     });
 
-    function drawImage() {
-        context.drawImage(img, 0, 0);
+    function drawImages() {
+        for(let i = 0; i < images.length; i++) {
+            context.drawImage(images[i], images[i].xCoord, images[i].yCoord);
+        }  
     }
 
-    function redraw() {   
-        context.save();
-        context.resetTransform();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.restore();
-        
-        drawImage();
-        window.requestAnimationFrame(redraw);
-        //todo less idle animations;
-        //todo object coords;
+    function drawCoords() {
+        const lineHeight = 24;
+        for(let i = 0; i < images.length; i++) {
+            const message = `Файл "${images[i].fileName}"; координаты x: ${images[i].xCoord}, y: ${images[i].xCoord}`;
+            context.fillText(message, 0, lineHeight * i);
+        }  
     }
 
-    redraw();
+    function draw() {   
+        context.clearRect(0, 0, canvas.width, canvas.height);        
+        drawImages();
+        drawCoords();
+    }
+
+    draw();
 });
+
+        //todo less idle animations; ++
+        //todo image coords; ++
+        //todo multiple images movable separately (optional); ++

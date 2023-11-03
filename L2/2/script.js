8884cb9ef3b3767f347a44ba90537be8c23bc99d
Maxim Laptev <justsaynya@gmail.com>
2023-11-03 22:29:45 +0300

    L2.2 refactored

--- L2/2/script.js
+++ L2/2/script.js
@@ -1,136 +1,137 @@
-function initCanvas() {
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    context.lineJoin = "round";
-    context.lineCap = "round";
-    return [canvas, context];
-}
+window.addEventListener("load", () => {    
+    const [canvas, context] = initCanvas();
+    initDrawingParamsControls();
+    setMouseEventListenersForDrawing();
+    initDialogNewControls();
+    initDialogOpenControls();
+    initDialogSaveControls();
 
-//update element context according to drawing inputs
-function updateContext(context) {
-    const colorPicker = document.getElementById("color");
-    const widthPicker = document.getElementById("drawWidth");
-    context.strokeStyle = colorPicker.value;
-    context.lineWidth = widthPicker.value;
-}
-
-function initDrawing(canvas, context) {
-    updateContext(context);
-
-    let isDrawing = false;    
-    canvas.addEventListener('mousedown', (e) => {
-        isDrawing = true;
-        context.beginPath();
-        context.moveTo(e.offsetX, e.offsetY);
-        context.lineTo(e.offsetX + 1, e.offsetY + 1);
-        context.stroke();
-    });
-    canvas.addEventListener('mouseup', (e) => {
-        if(isDrawing) {
-            isDrawing = false;
-            context.lineTo(e.offsetX, e.offsetY);
-            context.stroke();
-            context.closePath();
-        }
-    });
-    window.addEventListener('mouseup', () => {
-            isDrawing = false;
-    });
-
-    canvas.addEventListener('mouseout', (e) => {
-        if(isDrawing) {
-            context.lineTo(e.offsetX, e.offsetY);
-            context.stroke();
-            context.closePath();
-        }
-    });
-    canvas.addEventListener('mouseover', (e) => {
-        if(isDrawing) {
-            context.beginPath();
-            context.moveTo(e.offsetX, e.offsetY);
-        }
-    });
-    canvas.addEventListener('mousemove', (e) => {
-        if(isDrawing) {
-            context.lineTo(e.offsetX, e.offsetY);
-            context.stroke();
-        }
-    });
-}
-
-function initDrawingControls(context) {
-    const colorPicker = document.getElementById("color");
-    const widthPicker = document.getElementById("drawWidth");
-
-    colorPicker.addEventListener("change", (e) => context.strokeStyle = e.target.value);
-    widthPicker.addEventListener("change", (e) => context.lineWidth = e.target.value);
-}
-
-function initCreateDialog(canvas, context) {
-    const newButton = document.getElementById("new");
-    const newDialog = document.getElementById("newModal");
-    const newCreate = document.getElementById("newCreate");
-    const newCancel = document.getElementById("newCancel");
-
-    const inputs = newDialog.getElementsByClassName("dialog-input");
-
-    newButton.addEventListener("click", () => newDialog.classList.toggle("hidden"));
-    newCancel.addEventListener("click", () => newDialog.classList.toggle("hidden"));
-    newCreate.addEventListener("click", () => {
-        canvas.width = inputs[0].value;
-        canvas.height = inputs[1].value;
-        
-        updateContext(context);
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        newDialog.classList.toggle("hidden");
-    });
-}
-
-function initOpenDialog(canvas, context) {
-    function drawImage() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        updateContext(context);
-        context.drawImage(img, 0, 0);
+    function initCanvas() {
+        const canvas = document.getElementById("canvas");
+        const context = canvas.getContext("2d");
+        context.lineJoin = "round";
+        context.lineCap = "round";
+        return [canvas, context];
     }
 
-    const img = new Image;
-    const inputFile = document.getElementById("file");
-    inputFile.addEventListener("change",  (e) => {
-        img.addEventListener("load", drawImage);
-        img.src = URL.createObjectURL(e.target.files[0]);
-    });
+    function getDrawingParameters() {
+        return [
+            document.getElementById("color").value,
+            document.getElementById("drawWidth").value
+        ];
+    }
 
-    const openButton = document.getElementById("open");
-    openButton.addEventListener("click", () => inputFile.click());
-}
+    function setDrawingParameters() {
+        [context.strokeStyle, context.lineWidth] = getDrawingParameters();
+    }
 
-function initSaveDialog(canvas) {
-    const saveButton = document.getElementById("save");
-    const saveDialog = document.getElementById("saveModal");
-    const saveCreate = document.getElementById("saveCreate");
-    const saveCancel = document.getElementById("saveCancel");
+    function initDrawingParamsControls() {
+        const colorControl = document.getElementById("color");
+        const widthControl = document.getElementById("drawWidth");
+        colorControl.addEventListener("change", setDrawingParameters);
+        widthControl.addEventListener("change", setDrawingParameters);
 
-    const fileNameImput = document.getElementById("fileName");
-    const fileTypeInput = document.getElementById("type");
+        setDrawingParameters();
+    }
 
-    const link = document.createElement("a");
+    function setMouseEventListenersForDrawing() {
+        let isDrawing = false;    
+        canvas.addEventListener('mousedown', (e) => {
+            isDrawing = true;
+            context.beginPath();
+            context.moveTo(e.offsetX, e.offsetY);
+            context.lineTo(e.offsetX + 1, e.offsetY + 1);
+            context.stroke();
+        });
+        canvas.addEventListener('mouseup', (e) => {
+            if(isDrawing) {
+                isDrawing = false;
+                context.lineTo(e.offsetX, e.offsetY);
+                context.stroke();
+                context.closePath();
+            }
+        });
+        window.addEventListener('mouseup', () => {
+                isDrawing = false;
+        });
+        canvas.addEventListener('mouseout', (e) => {
+            if(isDrawing) {
+                context.lineTo(e.offsetX, e.offsetY);
+                context.stroke();
+                context.closePath();
+            }
+        });
+        canvas.addEventListener('mouseover', (e) => {
+            if(isDrawing) {
+                context.beginPath();
+                context.moveTo(e.offsetX, e.offsetY);
+            }
+        });
+        canvas.addEventListener('mousemove', (e) => {
+            if(isDrawing) {
+                context.lineTo(e.offsetX, e.offsetY);
+                context.stroke();
+            }
+        });
+    }
 
-    saveButton.addEventListener("click", () => saveDialog.classList.toggle("hidden"));
-    saveCancel.addEventListener("click", () => saveDialog.classList.toggle("hidden"));
-    saveCreate.addEventListener("click", () => {
-        link.download = fileNameImput.value + fileTypeInput.options[fileTypeInput.selectedIndex].text;
-        link.href = canvas.toDataURL(fileTypeInput.value);
-        link.click();
-        saveDialog.classList.toggle("hidden");
-    });
-}
+    function initDialogNewControls() {
+        const newButton = document.getElementById("new");
+        const newDialog = document.getElementById("newModal");
+        const newCreate = document.getElementById("newCreate");
+        const newCancel = document.getElementById("newCancel");
 
-window.addEventListener("load", () => {
-    const [canvas, context] = initCanvas();
-    initDrawing(canvas, context);
-    initDrawingControls(context);
-    initCreateDialog(canvas, context);
-    initOpenDialog(canvas, context);
-    initSaveDialog(canvas);
+        const inputs = newDialog.getElementsByClassName("dialog-input");
+
+        newButton.addEventListener("click", () => newDialog.classList.toggle("hidden"));
+        newCancel.addEventListener("click", () => newDialog.classList.toggle("hidden"));
+        newCreate.addEventListener("click", () => {
+            canvas.width = inputs[0].value;
+            canvas.height = inputs[1].value;
+            
+            setDrawingParameters(context);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            newDialog.classList.toggle("hidden");
+        });
+    }
+
+    function initDialogOpenControls() {
+        function drawImage() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            setDrawingParameters(context);
+            context.drawImage(img, 0, 0);
+        }
+
+        const img = new Image;
+        const inputFile = document.getElementById("file");
+        inputFile.addEventListener("change",  (e) => {
+            img.addEventListener("load", drawImage);
+            img.src = URL.createObjectURL(e.target.files[0]);
+        });
+
+        const openButton = document.getElementById("open");
+        openButton.addEventListener("click", () => inputFile.click());
+    }
+
+    function initDialogSaveControls() {
+        const saveButton = document.getElementById("save");
+        const saveDialog = document.getElementById("saveModal");
+        const saveCreate = document.getElementById("saveCreate");
+        const saveCancel = document.getElementById("saveCancel");
+
+        const fileNameImput = document.getElementById("fileName");
+        const fileTypeInput = document.getElementById("type");
+
+        const link = document.createElement("a");
+
+        saveButton.addEventListener("click", () => saveDialog.classList.toggle("hidden"));
+        saveCancel.addEventListener("click", () => saveDialog.classList.toggle("hidden"));
+        saveCreate.addEventListener("click", () => {
+            link.download = fileNameImput.value + fileTypeInput.options[fileTypeInput.selectedIndex].text;
+            link.href = canvas.toDataURL(fileTypeInput.value);
+            link.click();
+            saveDialog.classList.toggle("hidden");
+        });
+    }
 });
